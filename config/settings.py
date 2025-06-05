97d348a71e07b871a9451f285486e814f6fddd29
luifervillalba <villalba.luifer@gmail.com>
2025-06-05 16:53:56 -0300

    Preparing for deployment

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-6m64x^%&t5xyomhi=%j70++-l4r)gn$cv+563l-wl@thns4@px
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
